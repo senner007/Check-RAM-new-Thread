710b2abbe4a1086064605ef7f141fe60951d8f84
senner007 <nielshtg@hotmail.com>
2018-10-26 20:37:10 +0200

    giti init

---
+++ MyAppTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyApp;
+
+namespace MyAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            // ClassToTest classToTest = new ClassToTest();
+
+            // string newString = classToTest.ConcatStrings("Hello", "World");
+
+            // //Assert
+
+            // Assert.AreEqual(newString, "HelloWorld");
+
+        }
+    }
+}
